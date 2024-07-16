35ed4696aecf133a3429504efa924619468c809d
Prathamesh <prathameshpimpalkar2002@gmail.com>
2024-07-16 11:10:48 +0530

    Added Google & Github Provider

--- app/libs/route.ts
+++ app/libs/route.ts
@@ -1,4 +1,6 @@
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 
 export const NEXT_AUTH = {
 providers: [
@@ -16,7 +18,15 @@ providers: [
                 email:"abc@gmail.com"
             }
         }
-    })
+    }),
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID || "",
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
+      }),
+    GitHubProvider({
+        clientId: process.env.GITHUB_ID || "",
+        clientSecret: process.env.GITHUB_SECRET || ""
+      })
 ],
 secret: process.env.NEXTAUTH_SECRET,
 callbacks: {
